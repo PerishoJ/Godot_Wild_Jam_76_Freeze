58b46f85756e5a337c88e7d2f7ed239754853251
Clément <leconte.clement@orange.fr>
2024-12-20 20:33:00 +0100

    test commit to test if access to repository

---
+++ sandbox/snowball.gd
@@ -0,0 +1,17 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print("Snowball position is: %s" % position)
+
+
+
+
+func _on_Snowball_body_entered(body):
+	body.remove_from_game()
