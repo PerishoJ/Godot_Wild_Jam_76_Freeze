8861e1e0c79c8756c7eb9c2c46dc7600d18e5f99
joe <cafejoe61@google.com>
2024-12-21 12:56:44 -0600

    Fixed some wonky merge weirdness

--- characters/player/player_controller.gd
+++ characters/player/player_controller.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
 
+# Animation Controllers
+@onready var animTree : PenguinAnimCtrl = $Penguin
+
+# Movement controls
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
+var is_in_movement: bool
 
 # This is updated every frame to match what angle the camera is at compared to the object.
 var _camera_rotation : Vector3 = Vector3( 0 , 0 , 0 )
@@ -12,17 +17,17 @@ func _process(delta: float) -> void:
 
 func if_the_player_wants_to_quite():
   if Input.is_key_pressed(KEY_ESCAPE):
-	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
-	get_tree().quit()
+    get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+    get_tree().quit()
 
 func _physics_process(delta: float) -> void:
   # Add the gravity.
   if not is_on_floor():
-	velocity += get_gravity() * delta
+    velocity += get_gravity() * delta
 
   # Handle jump.
   if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	velocity.y = JUMP_VELOCITY
+    velocity.y = JUMP_VELOCITY
 
   # Get the input direction and handle the movement/deceleration.
   # As good practice, you should replace UI actions with custom gameplay actions.
@@ -30,18 +35,37 @@ func _physics_process(delta: float) -> void:
   # Rotate to match the input
   var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
   # Now rotate one more time to match the angle of the camera
-  print("camera rotation is " + str(_camera_rotation))
-  direction = direction.rotated(Vector3.UP, _camera_rotation.y)
-  if direction:
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+  
+  if velocity:
+    is_in_movement = true
   else:
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-	velocity.z = move_toward(velocity.z, 0, SPEED)
+    is_in_movement = false
+  move_and_slide()
+  direction = direction.rotated(Vector3.UP, _camera_rotation.y )
 
+
+  if direction:
+    velocity.x = direction.x * SPEED
+    velocity.z = direction.z * SPEED
+  else:
+    velocity.x = move_toward(velocity.x, 0, SPEED)
+    velocity.z = move_toward(velocity.z, 0, SPEED)
+  _emit_movement_signals(direction)
   move_and_slide()
 
 
+func _emit_movement_signals(direction):
+  if velocity:
+    is_in_movement = true
+  else:
+    is_in_movement = false
+
+  var isRunning = direction and is_on_floor()
+  if(isRunning):
+    animTree.penguin_action="RUNNING"
+  else:
+    animTree.penguin_action="IDLE"
+
 func _on_camera_rig_camera_rotation_event(rotation):
   _camera_rotation=rotation
-  print(" Received " + str(rotation) + " and value is now " + str(_camera_rotation))
+ # Rotate half circle because we're behind
